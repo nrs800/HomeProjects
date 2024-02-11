158ff42c4e1966a66e262ba6008248b9e597fea5
Nathanael Seay <nrs800@comcast.net>
2024-02-11 08:58:08 -0500

    Fixing the Shaper function and completing the bearing calls

--- Anomaly_Shaper.py
+++ Anomaly_Shaper.py
@@ -2,7 +2,7 @@
 
 
 
-def Shaper():
+def Shaper(bearing):
 
     import glob
     import pandas as pd
@@ -16,12 +16,49 @@ def Shaper():
     filenames2 = glob.glob(path2 + "/*.39")
     filenames= filenames1+filenames2
 
-    dfs = pd.DataFrame()
+    dfs1 = pd.DataFrame()
+    dfs2 = pd.DataFrame()
+    dfs3 = pd.DataFrame()
+    dfs4 = pd.DataFrame()
+    
     for filename in filenames:
         df=pd.read_csv(filename, sep='\t', header= None)
     
-        df1 = df.iloc[0]
+        #df1 = df.iloc[0]
+        df1 = df.iloc[:, lambda df: [0]]
     
-        dfs= pd.concat([dfs, df1], axis = 1)
+        dfs1= pd.concat([dfs1, df1], axis = 1)
+        
+        df2 = df.iloc[:, lambda df: [1]]
     
-    melt = pd.melt(dfs)
+        dfs2= pd.concat([dfs2, df2], axis = 1)
+        
+        df3 = df.iloc[:, lambda df: [2]]
+    
+        dfs3= pd.concat([dfs3, df3], axis = 1)
+        
+        df4 = df.iloc[:, lambda df: [4]]
+    
+        dfs4= pd.concat([dfs4, df4], axis = 1)
+    
+    # melt1 = pd.melt(dfs1)
+    # melt2 = pd.melt(dfs2)
+    # melt3 = pd.melt(dfs3)
+    # melt4 = pd.melt(dfs4)
+    
+    if (bearing==1):
+        melt = pd.melt(dfs1)
+        
+    if (bearing==2):
+        melt = pd.melt(dfs2)
+        
+    if (bearing==3):
+        melt = pd.melt(dfs1)
+            
+    if (bearing==4):
+        melt = pd.melt(dfs2)
+        
+    return melt
+
+
+Data = Shaper(1)    
