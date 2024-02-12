9332ad9bf8dd016df4d449f831858304da4964c7
Nathanael Seay <nrs800@comcast.net>
2024-02-12 00:26:35 -0500

    train_test split added

--- Anomaly.py
+++ Anomaly.py
@@ -46,3 +46,20 @@ ax.plot(wave_data, label = 'bearing', linewidth = 1)
 
 plot = wave_data.plot()
 
+
+
+train_amount= len(wave_data)*.8
+
+test_amount=len(wave_data)
+
+train_amount = int(train_amount)
+
+test_amount = int(test_amount)
+
+train = wave_data.iloc[1:train_amount]
+
+test = wave_data.iloc[train_amount:test_amount]
+
+
+print("Training dataset shape:", train.shape)
+print("Test dataset shape:", test.shape)
