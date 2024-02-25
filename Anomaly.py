ca5caa6d1c829d78615461c8950c0ae8660c451a
Nathanael Seay <nrs800@comcast.net>
2024-02-24 23:26:09 -0500

    Working hyper param tuner model without layer tuning

--- Anomaly.py
+++ Anomaly.py
@@ -23,17 +23,13 @@ import tensorflow as tf
 from tensorflow import keras
 tf.keras.utils.set_random_seed(10)
 from tensorflow.keras import layers
-# random.seed(seed)
-# np.random.seed(seed)
-# tf.random.set_seed(seed)
 
-# tf.logging.set_verbosity(tf.logging.ERROR)
 
 from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector
 from keras.models import Model
 from keras import regularizers
 
-from kerastuner.tuners import RandomSearch
+from keras_tuner.tuners import RandomSearch
 
 import glob
 #import pandas as pd
@@ -48,12 +44,7 @@ filenames2 = glob.glob(path2 + "/*.39")
 filenames= filenames1+filenames2
 
 
-
 def Shaper(bearing, filenames):
-
-
-
-  
 
     dfs1 = pd.DataFrame()
     dfs2 = pd.DataFrame()
@@ -80,10 +71,6 @@ def Shaper(bearing, filenames):
     
         dfs4= pd.concat([dfs4, df4], axis = 1)
     
-    # melt1 = pd.melt(dfs1)
-    # melt2 = pd.melt(dfs2)
-    # melt3 = pd.melt(dfs3)
-    # melt4 = pd.melt(dfs4)
     
     if (bearing==1):
         melt = pd.melt(dfs1)
@@ -102,9 +89,7 @@ def Shaper(bearing, filenames):
     return melt
 
 
-
 bearing = 4
-
 
 wave_data = Shaper(bearing, filenames)
 plt.plot(wave_data, label='bearing', linewidth=1)
@@ -126,20 +111,6 @@ X_test = scaler.transform(test.values.reshape(-1, 1))
 scaler_filename = "scaler_data"
 joblib.dump(scaler, scaler_filename)
 
-# define the autoencoder network model
-# def autoencoder_model(X):
-#     inputs = Input(shape=(X.shape[1], 1))  
-#     L1 = LSTM(64, activation='relu', return_sequences=True, kernel_regularizer=regularizers.l2(0.00))(inputs)
-#     L2 = LSTM(16, activation='relu', return_sequences=True)(L1)
-#     L3 = LSTM(4, activation='relu', return_sequences=False)(L2)
-#     L4 = RepeatVector(X.shape[1])(L3)
-#     L5 = LSTM(4, activation='relu', return_sequences=True)(L4)
-#     L6 = LSTM(16, activation='relu', return_sequences=True)(L5)
-#     L7 = LSTM(64, activation='relu', return_sequences=True)(L6)
-#     output = TimeDistributed(Dense(1, activation='relu'))(L7)  
-#     model = Model(inputs=inputs, outputs=output)
-#     return model
-
 # Define the autoencoder model
 def build_autoencoder(hp):
     inputs = keras.Input(shape=(timesteps, input_dim))
@@ -159,7 +130,7 @@ def build_autoencoder(hp):
     
     # Build the autoencoder model
     autoencoder = keras.Model(inputs, outputs)
-    autoencoder.compile(optimizer=keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])),
+    autoencoder.compile(optimizer=tf.keras.optimizers.legacy.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])),
                         loss='mse')
     return autoencoder
 
@@ -188,15 +159,6 @@ best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
 best_model = tuner.hypermodel.build(best_hps)
 history= best_model.fit(X_train, X_train, epochs=10, validation_split=0.2).history
 
-
-# model = autoencoder_model(X_train)
-# model.compile(optimizer='adam', loss='mse')
-# model.summary()
-
-# nb_epochs = 50
-# batch_size = 512
-# history = model.fit(X_train, X_train, epochs=nb_epochs, batch_size=batch_size,
-#                     validation_split=0.05).history
 
 fig, ax = plt.subplots(figsize=(14, 6), dpi=80)
 ax.plot(history['loss'], 'b', label='Train', linewidth=2)
